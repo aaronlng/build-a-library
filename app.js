8b2f291f0c864035206123064af60705269ff6aa
Aaron Long <aaronlng05@gmail.com>
2020-06-02 14:29:04 -0400

    add Movie class

--- app.js
+++ app.js
@@ -56,3 +56,21 @@ class Book extends Media {
         return this._pages;
     }
 }
+
+//Movie Class
+
+class Movie extends Media {
+    constructor(director, title, runTime) {
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+    }
+
+    get director() {
+        return this._director;
+    }
+
+    get runTime() {
+        return this._runTime;
+    }
+}
